24ad915b00c12e258fd4b8a2af187e29e04fb5aa
Ruchi Bhandari <98799210+ruchibhandarigithub@users.noreply.github.com>
2023-06-02 12:01:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let div = document.getElementByID("output");
+div.innerText = function(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve('Hello, world!');
+		},1000);
+	})
+}
